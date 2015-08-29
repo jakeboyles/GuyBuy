8dd3db8096b9e231a325a5a5d83551f0c4c290c3
Hello <hello@straphq.com>
2015-08-28 21:27:30 -0400

    fixing

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="col-md-6">
         <ul>
-        <li><a href="#">Home</a></li>
+        <li><a href="/">Home</a></li>
         <li><a href="#">Contact</a></li>
 
         <li><img src="/images/ohio.png"> Made In Ohio</li>
