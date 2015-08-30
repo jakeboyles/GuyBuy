31906967c371de5307872df16f7e805c0e659e51
Hello <hello@straphq.com>
2015-08-30 19:20:43 -0400

    fixing facebook

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     'facebook' => [
     'client_id' => '821633007950134',
     'client_secret' => 'ddf9a0b2774c85bb7c86528f8b0fd997',
-    'redirect' => 'http://localhost:8000/facebook/callback',
+    'redirect' => 'http://buysguys.co/facebook/callback',
 ],
 
 ];
