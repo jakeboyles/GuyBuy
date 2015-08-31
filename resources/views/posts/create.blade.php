1bfb81b4c5d234061843d9f4efee6e152158bbb6
Hello <hello@straphq.com>
2015-08-30 21:08:02 -0400

    fixing h2

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -91,7 +91,7 @@
             <div class="form-group">
                 <div class="col-md-6 col-md-offset-4">
                     <button type="submit" class="btn btn-primary">
-                        Create
+                        Create Post
                     </button>
                 </div>
             </div>
