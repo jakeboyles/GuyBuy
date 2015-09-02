dc69b353f153ee37ed6c86c1cdf2e7a26ad8d5ff
Hello <hello@straphq.com>
2015-09-01 21:55:16 -0400

    fixing stuff

--- resources/views/community/home.blade.php
+++ resources/views/community/home.blade.php
@@ -48,8 +48,8 @@
 					<p class="bodyText">{{str_limit($post->body,90)}}</p>
 
 					<p class="commentCount pull-left">{{$post->commentsCount()}} <i class="fa fa-comments"></i></p>
-			<!-- 		<p>{{$post->author()->first()->name}} in {{$post->community()->first()->name}}, {{$post->community()->first()->state}}</p>
-			 -->		</div>
+					<p><i class="fa fa-home"></i> {{$post->community()->first()->name}}, {{$post->community()->first()->state}}</p>
+					</div>
 				</div>
 			</div>
 		</div>
